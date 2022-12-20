8a12fd33cae680bb81a881d89e4a6d903917fc0f
Stephen Epstein <epste145@umn.edu>
2022-12-19 20:55:13 -0600

    ipynb

--- ex.py
+++ ex.py
@@ -1,3 +1,13 @@
-1+2
+print(1+2)
+3+4
 
 print("hi!")
+
+a = 1
+b = 2.345
+
+f"my number is {a} and I love it! I also love {b:.2f}"
+from google.cloud import bigquery
+
+client = bigquery.Client(project = project_id)
+df = client.query(query).to_dataframe()
